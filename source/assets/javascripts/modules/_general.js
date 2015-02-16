2227f1517af024454183d5c9d2db9b1290a88509
Antonio Junior <junyor@hotmail.co.uk>
2015-02-16 19:44:44 -0200

    Creating method to make a parallax on header background.

--- source/assets/javascripts/modules/_general.js
+++ source/assets/javascripts/modules/_general.js
@@ -5,11 +5,30 @@ antoniojunyor.general = (function(){
 
   function init() {
     defTotalHeight();
+    parallaxHeader();
   }
 
   function defTotalHeight() {
     $('.aj-header').height(window.innerHeight);
   };
+
+  function parallaxHeader() {
+    $('.aj-header').each(function() {
+      var $obj = $(this);
+
+      $(window).scroll(function() {
+        var yPos = -($(window).scrollTop() / 5);
+
+        if (window.innerWidth <= 998) {
+          var bgPos = yPos + 'px';
+        } else {
+          var bgPos = (-500 + yPos) + 'px';
+        };
+
+        $obj.css('background-position-y', bgPos);
+      });
+    });
+  }
 
   return {
     init: init
