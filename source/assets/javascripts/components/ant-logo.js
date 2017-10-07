fb4c093fce3fd1ee08a59f4462810727c2b63ccb
Antonio Junior <junyor@hotmail.co.uk>
2017-10-07 17:01:09 -0300

    Creating home page with logo inside.

---
+++ source/assets/javascripts/components/ant-logo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AntLogo = () => {
+  return (
+    <svg className="ant-svg-logo" version="1.1" x="0" y="0" viewBox="0 0 1000 1000">
+      <g className="ant-container-svg">
+        <polygon stroke="#000" strokeMiterlimit="10" points="309.4,560.5 384.4,560.5 346.6,357.7"/>
+        <path stroke="#000" strokeMiterlimit="10" d="M920.5,315.5v403h-63v-403h-81v-47h198v-249h-949v949h949v-653H920.5z M413.2,718.5l-20.6-113H301l-21.7,113h-58.9l95.6-450h60.6l96.1,450H413.2z M741.5,718.5H701L571.5,411.4v307.1h-53v-450H561l127.5,299v-299h53V718.5z"/>
+      </g>
+    </svg>
+  );
+}
+
+export default AntLogo;
---
+++ source/assets/javascripts/components/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import AntLogo from './ant-logo';
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <AntLogo/>
+    );
+  }
+}
