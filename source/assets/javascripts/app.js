01072b41a1aea5a6cf7788bd70f06cf56f03b908
Antonio Junior <junyor@hotmail.co.uk>
2017-10-07 16:58:51 -0300

    Creating route to home page.

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -1,1 +1,17 @@
-console.log('hey apple');
+import '../stylesheets/app.sass';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
+import Home from './components/home';
+
+const container = document.querySelector('.ant-body-container');
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/" component={Home}/>
+    </div>
+  </Router>
+, container);
